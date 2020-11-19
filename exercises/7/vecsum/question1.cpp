37565185d66d73ac0b0d1fd2d61ecde17afb57fb
Pavel Macak <pavel.macak@fs.cvut.cz>
2020-11-19 11:12:40 +0100

    started ex7

---
+++ exercises/7/vecsum/question1.cpp
@@ -0,0 +1,19 @@
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <numeric>
+#include <random>
+#include <vector>
+#include <cassert>
+#include <chrono>
+
+int main(int argc, char const *argv[])
+{
+    assert(argc==2);
+    int n = atoi(argv[1]);
+    assert(n>0)
+    std::vector<int> v(n,0);
+    for
+    
+    return 0;
+}
