18d2093eff2bcf931df4d901b172fda624864873
Pavel Macak <pavel.macak@fs.cvut.cz>
2020-11-19 10:40:02 +0100

    Initial commit of ex7 files

---
+++ exercises/7/downloaded/library.cpp
@@ -0,0 +1,43 @@
+#include <vector>
+#include <iostream>
+#include <tuple>   
+#include <cassert>
+#include <numeric>
+#include <cmath>
+namespace tws{
+    void print_vector(std::vector<int> const & v)
+    {
+        std::cout<<"("<<v.size()<<") [";
+        for (decltype(v.size()) i = 0; i < v.size(); ++i) {
+            std::cout<<v[i]<<" ";
+        }
+        std::cout<<"]"<<std::endl;
+    }
+    void print_vector(std::vector<double> const & v)
+    {
+        std::cout<<"("<<v.size()<<") [";
+        for (decltype(v.size()) i = 0; i < v.size(); ++i) {
+            std::cout<<v[i]<<" ";
+        }
+        std::cout<<"]"<<std::endl;
+    }
+    void reverse_print_vector(std::vector<int> const & v)
+    {
+        std::cout<<"("<<v.size()<<") [";
+        for (decltype(v.size()) i = v.size()-1; i > 0;--i ) {
+            std::cout<<v[i]<<" ";
+        }
+        std::cout<<"]"<<std::endl;
+    }
+    std::tuple<double, double> calculate_mean_stdev(std::vector<double> const & v,const int nbtodiscard=5){
+        assert(v.size()>0);
+        assert(nbtodiscard> 0);
+        assert((int) v.size()>nbtodiscard);
+        int N=v.size();
+        auto mean = std::accumulate(v.begin()+nbtodiscard, v.end(), 0.0);
+        mean = mean / (N-nbtodiscard);
+        auto st_dev = std::accumulate(v.begin()+nbtodiscard, v.end(), 0.0,[mean](auto res, auto x){return res+(x-mean)*(x-mean);});
+        st_dev = std::sqrt(st_dev / (N-nbtodiscard-1));
+        return {mean,st_dev};
+    }
+}
